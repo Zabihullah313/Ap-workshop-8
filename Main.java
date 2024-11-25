81e309d30ecd43b9b5009aaf2f760e834ff2239e
Zabihullah <zabihullahmohammadi313@gmail.com>
2024-11-25 23:42:42 +0330

    after finishing the notebook class we create an object of that class.

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Notebook notebook = new Notebook();
+        Scanner scanner = new Scanner(System.in);
 
     }
 }
