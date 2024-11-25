302816e7181d3b97dd56af4d16ea7080b05ea35d
Zabihullah <zabihullahmohammadi313@gmail.com>
2024-11-25 23:45:29 +0330

    In a infinite loop we make some option by numbers for every actions.

--- Main.java
+++ Main.java
@@ -5,5 +5,14 @@ public class Main {
         Notebook notebook = new Notebook();
         Scanner scanner = new Scanner(System.in);
 
-    }
+        while (true) {
+            System.out.println("\n--- Personal Notebook ---");
+            System.out.println("1. Add Note");
+            System.out.println("2. Remove Note");
+            System.out.println("3. List Notes");
+            System.out.println("4. View Note");
+            System.out.println("5. Export Note");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
+        }
 }
