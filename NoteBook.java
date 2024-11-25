b0b88a1bc4a1c34f36ef659bba6c3ee49e44985e
Zabihullah <zabihullahmohammadi313@gmail.com>
2024-11-25 23:12:40 +0330

    we create a hashmap for our class and a string to save our file address or path and our constructor.

--- NoteBook.java
+++ NoteBook.java
@@ -1,3 +1,13 @@
-public class NoteBook {
+import java.util.HashMap;
+import java.util.Map;
 
-}
+    class Notebook {
+        private Map<String, Note> notes = new HashMap<>();
+        private final String saveFilePath = "notebook_data.ser";
+
+
+        public Notebook() {
+            loadNotes();
+        }
+
+
